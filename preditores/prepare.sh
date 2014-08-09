66d94d956e7665026d1a9fb3396aaee40c7ef7ca
Henrique Pinto Gomide <henriquepgomide@gmail.com>
2014-08-09 10:49:11 -0300

    Atualização do script

--- preditores/prepare.sh
+++ preditores/prepare.sh
@@ -5,19 +5,29 @@
 # Class start with 302 and ends with 486.
 
 # Navigate to folder
+
+
+AWKOPT1="'{$1=\"" 
+AWKOPT2="\" \"\t\"  $1}1'"
+
 cd logs
 
 for i in {302..305}
 do
+
+  CLASS="Class"$i
+
   # Remove the first and second lines
-  # awk 'NR > 2 { print }' < class$i.txt
+  sed '1,3d' <class$i.txt>class_$i.txt
 
   # Insert a column with the file name
-  awk -F, '{$1=++i FS "class$i"}1' OFS=, class$i.txt   
+  # awk -F, '{$1="NA "\t"  $1}1' OFS=, class_$i.txt >> class.final.$i.txt
+
+  awk -F, ${AWKOPT1}${CLASS}${AWKOPT2} OFS=, class_$i.txt >> class.final.$i.txt
+
+
+  rm -f class_$i.txt class$i.txt
+
 done
 
-
-
-http://unix.stackexchange.com/questions/37790/how-do-i-delete-the-first-n-lines-of-an-ascii-file-using-shell-commands
-http://www.theunixschool.com/2012/11/awk-examples-insert-remove-update-fields.html
-
+cat *.txt >> log.txt
