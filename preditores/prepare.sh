dd17cd81c1ac9b6726ad27c09503c4b094d146bf
Henrique Gomide <henriquepgomide@gmail.com>
2014-08-09 23:10:19 -0300

    Atualização do script para pre processar os arquivos de log. It Works :)

--- preditores/prepare.sh
+++ preditores/prepare.sh
@@ -1,33 +1,20 @@
 #!/bin/sh
 
-# Remove lines, insert column with the filename and merge all of them.
-
-# Class start with 302 and ends with 486.
-
-# Navigate to folder
-
-
-AWKOPT1="'{$1=\"" 
-AWKOPT2="\" \"\t\"  $1}1'"
-
+# go to logs folder
 cd logs
 
 for i in {302..305}
-do
-
-  CLASS="Class"$i
-
-  # Remove the first and second lines
+do  
+  # Remove the first and second line of every txt
   sed '1,3d' <class$i.txt>class_$i.txt
+  
+  # Insert class number 
+  awk -F, -v variable=$i '{$1=variable "\t"  $1}1' OFS=, class_$i.txt >> class.final.$i.txt
 
-  # Insert a column with the file name
-  # awk -F, '{$1="NA "\t"  $1}1' OFS=, class_$i.txt >> class.final.$i.txt
-
-  awk -F, ${AWKOPT1}${CLASS}${AWKOPT2} OFS=, class_$i.txt >> class.final.$i.txt
-
-
+  # Remove temporary files
   rm -f class_$i.txt class$i.txt
 
 done
 
+# Merge all files into log.txt
 cat *.txt >> log.txt
