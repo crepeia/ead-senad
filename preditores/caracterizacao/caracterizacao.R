b8986b8a14bd68afdc8eccc8351ef5c30ed893da
Henrique Pinto Gomide <henriquepgomide@gmail.com>
2014-10-18 11:51:24 -0300

    Criação do gráfico de participação nas atividades e código para plotar mais de um gráfico.

--- preditores/caracterizacao/caracterizacao.R
+++ preditores/caracterizacao/caracterizacao.R
@@ -4,8 +4,45 @@ library("psych")    # Função describe
 library("plyr")     # Função ldply
 library("ggplot2")  # Gráficos
 
+# Função Multiplot
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 # Abrir banco de dados de atividades ----
-sociodemografico  <- read.csv("../preditores/caracterizacao/sociodemografico.csv")
+sociodemografico  <- read.csv("preditores/caracterizacao/sociodemografico.csv")
 
 ## Retirar questões inuteis
 sociodemografico <- sociodemografico[, -c(40:86)]
@@ -126,18 +163,69 @@ round(sapply(notasForum, mean, na.rm = TRUE),2)
 round(sapply(notasForum, sd, na.rm = TRUE),2)
 
 # Preparacao dos dados para realizacao do grafico
+## Avaliar quem fez a atividade
 forum  <- lapply(notasForum, is.na)
+
+## Tabular os resultados
 forumTeste <- lapply(forum, table)
+
+## Criar dataframe
 dfForum <- ldply(forumTeste)
+
+## Nomes para as variáeveis do banco
 colnames(dfForum)  <- c("forum", "atividade", "falta")
 
+## Atribuir bons labels aos fóruns
 dfForum[1,1] <- "01"; dfForum[2,1] <- "02"; dfForum[3,1] <- "03"; dfForum[4,1] <- "04"; dfForum[5,1] <- "05"; dfForum[6,1] <- "06"; dfForum[7,1] <- "07";dfForum[8,1] <- "08";dfForum[9,1] <- "09";dfForum[10,1] <- "10";dfForum[11,1] <- "11";dfForum[12,1] <- "12";dfForum[13,1] <- "13";dfForum[14,1] <- "14";dfForum[15,1] <- "15";
-
 dfForum$forum  <- as.factor(dfForum$forum)
 
-# Grafico
-plot(dfForum[,2], xlab="Fóruns", ylab="Número de acessos", type = "l", col = "blue", xlim=c(1,15))
+# Gráfico de participação nos fórusn - Bar plot - ggplot2
+graphForuns <- ggplot(data = dfForum, aes(x = forum, y = atividade)) + geom_bar(stat="identity", fill = "#CCCCCC", colour="#666666") + xlab("Fórum") + ylab("Participação") + coord_cartesian(ylim=c(0,4500)) + theme_bw() + theme(axis.title.x = element_text(face="bold", size=14), axis.text.x  = element_text(vjust=0.5, size=14), axis.title.y = element_text(face="bold", size=14), axis.text.y  = element_text(vjust=0.5, size=14))
 
-# Bar plot - ggplot2
-ggplot(data = dfForum, aes(x = forum, y = atividade)) + geom_bar(stat="identity", fill="navy") + xlab("Fórum") + ylab("Participação") + theme_bw()
+# Atividades Colaborativas ------
+# ===============================
 
+## Selecionar somente os foruns - Faltam questões ainda: Recuperação dos módulos 3 e 4.
+## Dúvida - O que são as recuperações gerais?
+notasAticol <- notas[, c("ativcolm1", "ativcolm1r","ativcolm2", "ativcolm2r", "ativcolm3", "ativcolm4")]
+
+## Substituir nota maior de quem fez recuperação
+
+### Atividade 1
+notasAticol$ativcolm1 <- ifelse(is.na(notasAticol$ativcolm1r), notasAticol$ativcolm1, notasAticol$ativcolm1r)
+### Atividade 2
+notasAticol$ativcolm2 <- ifelse(is.na(notasAticol$ativcolm2r), notasAticol$ativcolm2, notasAticol$ativcolm2r)
+
+### Implementar para atividades 3 e 4
+
+### Gráfico de boxplots com as notas
+notasAticol <- notasAticol[, c("ativcolm1", "ativcolm2", "ativcolm3", "ativcolm4")]
+
+# Preparacao dos dados para realizacao do grafico
+## Avaliar quem fez a atividade
+aticol  <- lapply(notasAticol, is.na)
+
+## Tabular os resultados
+aticolTeste <- lapply(aticol, table)
+
+## Criar dataframe
+dfAticol <- ldply(aticolTeste)
+
+## Nomes para as variáeveis do banco
+colnames(dfAticol)  <- c("Atividade", "Participou", "Nparticipou")
+
+## Atribuir bons labels às atividades colaborativas
+dfAticol[1,1] <- "01"; dfAticol[2,1] <- "02"; dfAticol[3,1] <- "03"; dfAticol[4,1] <- "04"
+
+# Gráfico de participação nos atividades - Bar plot - ggplot2
+graphAtiv <- ggplot(data = dfAticol, aes(x = Atividade, y = Participou)) + geom_bar(stat="identity", fill = "#CCCCCC", colour="#666666") + xlab("Atividade Colaborativa") + ylab("") + coord_cartesian(ylim=c(0,4500)) + theme_bw() + theme(axis.title.x = element_text(face="bold", size=14), axis.text.x  = element_text(vjust=0.5, size=14), axis.title.y = element_text(face="bold", size=14), axis.text.y  = element_text(vjust=0.5, size=14))
+
+
+# Questionários            ------
+# ===============================
+
+
+# Gráfico final            ------
+# ===============================
+
+multiplot(graphForuns, graphAtiv, cols=2)
