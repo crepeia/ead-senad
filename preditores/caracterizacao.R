ffbba366c4d963ae1a59486cad886adb755ddb0b
Henrique Gomide <henriquepgomide@gmail.com>
2014-10-20 16:28:58 -0200

    Orgnização da pasta preditores

--- preditores/caracterizacao.R
+++ preditores/caracterizacao.R
@@ -1,67 +1,231 @@
-# Definir diretório de trabalho
-setwd("caracterizacao/")
-
 # Pacotes
-library("tm")    # Trabalhar com strings
-library("psych") # Função describe
-library("reshape2")
+library("tm")       # Trabalhar com strings
+library("psych")    # Função describe
+library("plyr")     # Função ldply
+library("ggplot2")  # Gráficos
 
-# SOCIODEMOGRAFICO ----
-## Ler banco
-sociodemografico  <- read.csv("sociodemografico.csv")
+# Função Multiplot
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
 
-## Retirar questões desnecessárias
+# Abrir banco de dados de atividades ----
+sociodemografico  <- read.csv("preditores/caracterizacao/sociodemografico.csv")
+
+## Retirar questões inuteis
 sociodemografico <- sociodemografico[, -c(40:86)]
 
 ## Criar variável nome completo 
 sociodemografico$nomecompleto <- paste(sociodemografico$nome, sociodemografico$sobrenome)
 sociodemografico$nomecompleto <- stripWhitespace(tolower(sociodemografico$nomecompleto))
 
-## Converter para Character
-sociodemografico$nomecompleto  <- as.character(sociodemografico$nomecompleto)
 
 ## Criar banco somente com pessoas que consentiram participar
 sociodemografico  <- subset(sociodemografico, sociodemografico$termo == "Sim")
 
-# LOGS TRATADOS ----
-dfCast <- read.csv("logsTratados.csv")
+## Abrir banco de logs Tratados
+dfCast <- read.csv("caracterizacao/logsTratados.csv")
+
+# Mesclar bancos ----
 dfCast$.id  <- as.character(dfCast$.id)
 dfCast$nomecompleto  <- stripWhitespace(tolower(dfCast$.id))
 
-# NOTAS DOS ALUNOS ----
-notas  <- read.csv("notas.csv", na.strings="-")
-notas <- notas[, -c(5,15,16,17,18,20,21,31)]
-summary(notas)
+sociodemografico$nomecompleto  <- as.character(sociodemografico$nomecompleto)
 
-## Soma das notas em foruns
-notas$somaForum  <- notas$forum1 + notas$forum2 + notas$forum3 + notas$forum4 + notas$forum5 + notas$forum6 + notas$forum7 +notas$forum8 + notas$forum9 + notas$forum10 + notas$forum11 + notas$forum12 + notas$forum13 + notas$forum14 + notas$forum15
-
-##  Atividade dos alunos no fórum
-
-notas$f1 <- ifelse(is.na(notas$forum1), 0, 1)
-notas$f2 <- ifelse(is.na(notas$forum2), 0, 1)
-notas$f3 <- ifelse(is.na(notas$forum3), 0, 1)
-notas$f4 <- ifelse(is.na(notas$forum4), 0, 1)
-notas$f5 <- ifelse(is.na(notas$forum5), 0, 1)
-notas$f6 <- ifelse(is.na(notas$forum6), 0, 1)
-notas$f8 <- ifelse(is.na(notas$forum8), 0, 1)
-notas$f9 <- ifelse(is.na(notas$forum9), 0, 1)
-notas$f10 <- ifelse(is.na(notas$forum10), 0, 1)
-notas$f11 <- ifelse(is.na(notas$forum11), 0, 1)
-notas$f12 <- ifelse(is.na(notas$forum12), 0, 1)
-notas$f13 <- ifelse(is.na(notas$forum13), 0, 1)
-notas$f14 <- ifelse(is.na(notas$forum14), 0, 1)
-notas$f15 <- ifelse(is.na(notas$forum15), 0, 1)
-
-## Soma das notas dos questionários
-notas$somaQues <- notas$quesm1 + notas$quesm2 + notas$quesm3
-
-## Soma das notas das atividades colaborativas
-notas$somaAtivcol  <- notas$ativcolm1 + notas$ativcolm2 + notas$ativcolm4
-
-# Mesclar bancos ----
 ## Mesclar e preservar todos os valores
 bancoFinal  <- merge(sociodemografico, dfCast, by.x = "nomecompleto", by.y = "nomecompleto")
 
+## Notas
+notas  <- read.csv("caracterizacao/notas.csv")
 
 
+####################################################################################################
+## ARTIGO 1 -----------------------------------------------------------------------------------------
+## Descrição de um curso online para prevenção do uso de drogas para educadores de Escolas Públicas.
+####################################################################################################
+
+# Abrir banco de dados
+socioDemo  <- read.csv("praticas-profissionais/praticasprofissionais_df.csv")
+
+########################################
+# CARACTERÍSTICAS SOCIODEMOGRAFICAS ----
+########################################
+
+# Funçao para criar tabelas com porcentagem
+porcentagem <- function(var){
+  a  <- round(prop.table(table(var)),3)*100
+  print(cbind(sort(a, decreasing = TRUE)))
+}
+
+# Idade
+## Recodificar variável para numérica
+socioDemo$age  <- as.numeric(as.character(socioDemo$idade))
+## Excluir usuários que inseriram o ano de nascimento
+socioDemo$age <- ifelse(socioDemo$age > 70, NA, socioDemo$age)
+
+## Média de idade
+mean(socioDemo$age, na.rm = TRUE)
+## Desvio Padrão 
+sd(socioDemo$age, na.rm = TRUE)
+
+## Sexo
+porcentagem(socioDemo$sexo)
+
+## Escolaridade
+porcentagem(socioDemo$escolaridade)
+
+## Ocupaçao
+porcentagem(socioDemo$ocupacao)
+
+## Contato Anterior
+porcentagem(socioDemo$contatoanterior)
+
+## Onde lida
+porcentagem(socioDemo$lida.onde)
+
+## Motivo Curso
+porcentagem(socioDemo$motivocurso)
+
+########################################
+# SATISFAÇÃO COM O CURSO  ----
+########################################
+
+## Material Didático
+porcentagem(socioDemo$materialdidatico)
+
+## Prazo Atividades
+porcentagem(socioDemo$prazoatividades)
+
+## Interação entre pares
+porcentagem(socioDemo$interacaopares)
+
+## Organização curso
+porcentagem(socioDemo$organizacaocurso)
+
+## Importância da ajuda do tutor
+porcentagem(socioDemo$import.ajud.tutor)
+
+## Participação em outro curso
+porcentagem(socioDemo$part.outrocurso)
+
+########################################
+# NOTAS                             ----
+########################################
+
+## Ler banco com notas
+notas  <- read.csv("preditores/caracterizacao/notas.csv", dec = ",", na.strings=c("NA","-"))
+
+# Listar variáveis com notas
+summary(notas[,7:36])
+
+# Fóruns ------
+# =============
+
+# Selecionar somente os foruns
+notasForum  <- notas[, c("forum1", "forum2", "forum3","forum4","forum5","forum6","forum7","forum8","forum9","forum10","forum11","forum12","forum13","forum14","forum15")]
+
+# Notas medias dos foruns
+round(sapply(notasForum, mean, na.rm = TRUE),2)
+
+# Desvio padrão
+round(sapply(notasForum, sd, na.rm = TRUE),2)
+
+# Preparacao dos dados para realizacao do grafico
+## Avaliar quem fez a atividade
+forum  <- lapply(notasForum, is.na)
+
+## Tabular os resultados
+forumTeste <- lapply(forum, table)
+
+## Criar dataframe
+dfForum <- ldply(forumTeste)
+
+## Nomes para as variáeveis do banco
+colnames(dfForum)  <- c("forum", "atividade", "falta")
+
+## Atribuir bons labels aos fóruns
+dfForum[1,1] <- "01"; dfForum[2,1] <- "02"; dfForum[3,1] <- "03"; dfForum[4,1] <- "04"; dfForum[5,1] <- "05"; dfForum[6,1] <- "06"; dfForum[7,1] <- "07";dfForum[8,1] <- "08";dfForum[9,1] <- "09";dfForum[10,1] <- "10";dfForum[11,1] <- "11";dfForum[12,1] <- "12";dfForum[13,1] <- "13";dfForum[14,1] <- "14";dfForum[15,1] <- "15";
+dfForum$forum  <- as.factor(dfForum$forum)
+
+# Gráfico de participação nos fórusn - Bar plot - ggplot2
+graphForuns <- ggplot(data = dfForum, aes(x = forum, y = atividade)) + geom_bar(stat="identity", fill = "#CCCCCC", colour="#666666") + xlab("Fórum") + ylab("Participação") + coord_cartesian(ylim=c(0,4500)) + theme_bw() + theme(axis.title.x = element_text(face="bold", size=14), axis.text.x  = element_text(vjust=0.5, size=14), axis.title.y = element_text(face="bold", size=14), axis.text.y  = element_text(vjust=0.5, size=14))
+
+# Atividades Colaborativas ------
+# ===============================
+
+## Selecionar somente os foruns - Faltam questões ainda: Recuperação dos módulos 3 e 4.
+## Dúvida - O que são as recuperações gerais?
+notasAticol <- notas[, c("ativcolm1", "ativcolm1r","ativcolm2", "ativcolm2r", "ativcolm3", "ativcolm4")]
+
+## Substituir nota maior de quem fez recuperação
+
+### Atividade 1
+notasAticol$ativcolm1 <- ifelse(is.na(notasAticol$ativcolm1r), notasAticol$ativcolm1, notasAticol$ativcolm1r)
+### Atividade 2
+notasAticol$ativcolm2 <- ifelse(is.na(notasAticol$ativcolm2r), notasAticol$ativcolm2, notasAticol$ativcolm2r)
+
+### Implementar para atividades 3 e 4
+
+### Gráfico de boxplots com as notas
+notasAticol <- notasAticol[, c("ativcolm1", "ativcolm2", "ativcolm3", "ativcolm4")]
+
+# Preparacao dos dados para realizacao do grafico
+## Avaliar quem fez a atividade
+aticol  <- lapply(notasAticol, is.na)
+
+## Tabular os resultados
+aticolTeste <- lapply(aticol, table)
+
+## Criar dataframe
+dfAticol <- ldply(aticolTeste)
+
+## Nomes para as variáeveis do banco
+colnames(dfAticol)  <- c("Atividade", "Participou", "Nparticipou")
+
+## Atribuir bons labels às atividades colaborativas
+dfAticol[1,1] <- "01"; dfAticol[2,1] <- "02"; dfAticol[3,1] <- "03"; dfAticol[4,1] <- "04"
+
+# Gráfico de participação nos atividades - Bar plot - ggplot2
+graphAtiv <- ggplot(data = dfAticol, aes(x = Atividade, y = Participou)) + geom_bar(stat="identity", fill = "#CCCCCC", colour="#666666") + xlab("Atividade Colaborativa") + ylab("") + coord_cartesian(ylim=c(0,4500)) + theme_bw() + theme(axis.title.x = element_text(face="bold", size=14), axis.text.x  = element_text(vjust=0.5, size=14), axis.title.y = element_text(face="bold", size=14), axis.text.y  = element_text(vjust=0.5, size=14))
+
+
+# Questionários            ------
+# ===============================
+
+
+# Gráfico final            ------
+# ===============================
+
+multiplot(graphForuns, graphAtiv, cols=2)
