979a914d85dc4dfded0cfade12186588e54b1f64
Henrique Pinto Gomide <henriquepgomide@gmail.com>
2014-12-12 23:53:14 -0200

    Atualização dos mapas de acesso

--- preditores/relatorioMapa/relatorioEAD.R
+++ preditores/relatorioMapa/relatorioEAD.R
@@ -1,14 +1,17 @@
 ## Scrap HTML page
 # install.packages("ggmap")
+install.packages("rvest")
 
 ## Load package
+library(rvest)
 library(XML)
 library(ggmap)
 library(plyr)
+library(googleVis)
 
 
 ## Open HTML
-activity  <- html("/media//convidado/024C-1DE4//index.php_contextid=28&roleid=0&id=3&perpage=10200")
+activity  <- html("/media/hp/024C-1DE4//index.php_contextid=28&roleid=0&id=3&perpage=10200")
 
 ## Import data
 tables <- readHTMLTable(activity)
@@ -39,7 +42,8 @@ teste  <- tapply(df$access, df$city, table, simplify=FALSE)
 dfTeste <- ldply(teste, data.frame)
 
 
-## Geocoding
+## Geocoding ---
+## You should not use this commented lines below. Google API has query limit - 2500. You should use the data from csv.
 
 #geocodes1  <- geocode(dfTeste$.id[1:250])
 #geocodes2  <- geocode(dfTeste$.id[251:500])
@@ -67,15 +71,55 @@ geocodes <- rbind(geocodes1, geocodes2)
 rm(geocodes1, geocodes2)
 geocodes <- geocodes[, -1]
 
-newData  <- cbind(dfTeste, geocodes)
+## Bind dataframes
+geocodesFinal  <- cbind(dfTeste, geocodes)
+
+## Write csv
+# write.csv(geocodesFinal, "geocodesFinal.csv")
+
+
+
+###################
+## GGPLOT2 MAPS ##
+##################
+
+## Open Created csv
+geocodesFinal  <- read.csv("geocodesFinal.csv")
 
 ## Download Map
-map  <-  get_map(location=c(lon = -47.40306, lat = -18.48686), zoom = 6, maptype="toner",
-                source = "stamen", color = "color")
+map  <-  get_map(location=c(lon = -47.40306, lat = -18.48686), zoom = 6, maptype="roadmap",
+                 source = "google", color = "bw")
 
 ## Create ggplot2 graph
-sp.map.2012 <- ggmap(map, base_layer = ggplot(aes(x = lon, y = lat), data = newData), extent = "device")
+moodle.map <- ggmap(map, base_layer = ggplot(aes(x = lon, y = lat), data = geocodesFinal), extent = "device")
 
-## Plot map
-sp.map.2012 + geom_point(aes(colour=newData$Freq, fill = factor(newData$Var1)), alpha = 1/10, size = I(5))
+# Users
+moodle.map + stat_density2d(data = subset(geocodesFinal, geocodesFinal$Var1 == "Accessed") , aes(x = lon, y = lat,  fill = ..level.., alpha = ..level..), size = 0.01, bins = 50, geom = 'polygon') + scale_fill_gradient(low = "#F7FBFF", high = "#08306B") + scale_alpha(range = c(0.00, 0.15), guide = FALSE) +  theme(legend.position = "none", axis.title = element_blank(), text = element_text(size = 12))
 
+# Dropouts
+moodle.map + stat_density2d(data = subset(geocodesFinal, geocodesFinal$Var1 == "Not accessed") , aes(x = lon, y = lat,  fill = ..level.., alpha = ..level..), size = 0.01, bins = 50, geom = 'polygon') + scale_fill_gradient(low = "#FFF5EB", high = "#7F2704") + scale_alpha(range = c(0.00, 0.30), guide = FALSE) +  theme(legend.position = "none", axis.title = element_blank(), text = element_text(size = 12))
+
+
+# Dropouts vs Users
+moodle.map + stat_density2d(data = geocodesFinal, aes(x = lon, y = lat,  fill = ..level.., alpha = ..level..), size = 0.01, bins = 25, geom = 'polygon') + scale_fill_gradient(low = "#F7FBFF", high = "#08306B") + scale_alpha(range = c(0.00, 0.15), guide = FALSE) +  theme(legend.position = "none", axis.title = element_blank(), text = element_text(size = 12)) + facet_grid(. ~ Var1)
+
+
+###################
+## R GOOGLEVIS   ##
+###################
+
+geocodesFinal$gv  <- paste0(geocodesFinal$lat,":",geocodesFinal$lon)
+gvDf  <- geocodesFinal[1:100, c(2,6)]
+
+dropouts  <- gvisMap(gvDf, gvDf$gv,  
+                     options=list(showTip=TRUE, 
+                                  enableScrollWheel=TRUE,
+                                  mapType='terrain', 
+                                  useMapTypeControl=TRUE))
+
+
+## apgomide@gmail.com; Alexandre123; 
+## psn games BH
+## Anotar logins e senhas
+## Ver o passo-a-passo de como instalar
+## Instalar no videogame
