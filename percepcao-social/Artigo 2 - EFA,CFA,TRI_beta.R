491c2c77164afca955be7d42bce1982a3b2d6b86
Henrique Gomide <henriquepgomide@gmail.com>
2015-08-19 15:38:55 -0500

    Atualização do artigo 2 do script da escala de percepção social, criação do relatorio compilado do script

--- percepcao-social/Artigo 2 - EFA,CFA,TRI_beta.R
+++ percepcao-social/Artigo 2 - EFA,CFA,TRI_beta.R
@@ -2,7 +2,6 @@
 # 2nd PAPER     -----------
 ###########################
 
-
 ###########################
 # OPEN DATAFRAME
 ###########################
@@ -17,7 +16,7 @@ library(ltm)      # LTM analysis
 # Open Data Frame
 attitudesE  <- read.csv("atitudesEducadores_df.csv")
 
-# Convert into numeric
+# Convert items into numeric
 for (i in c(33:71)){
   attitudesE[,i]   <-  Recode(attitudesE[,i], "'Discordo totalmente'=1 ; 'Discordo'=2 ; 'Nem discordo, nem concordo' = 3; 'Concordo' = 4; 'Concordo totalmente' = 5; else = NA")
   attitudesE[,i]   <-  as.numeric(attitudesE[,i])  
@@ -47,20 +46,19 @@ polyAll <- polychoric(aScale)
 # Unrotated PCA
 faAll <- principal(polyAll$rho, nfactors = 2)
 print.psych(faAll, digits=2, cut= .4)
-#Rotated PCA
+# Rotated PCA
 faAll_rot <- principal(polyAll$rho, nfactors = 2, rotate="oblimin")
 print.psych(faAll_rot, digits=2, cut= .4)
-
-# Results - Items 9,11,12,13,18,21,22,25,30,31,36 need to be removed due to small loadings < (.4).
+# Results - Items 9,11,13,18,21,25,31,39 need to be removed due to small loadings < (.4).
 
 # SECOND EFA -------
 # Remove items with low loadings detected in previous step.
 shortScale  <- aScale[, -c(9,11,13,18,21,25,31, 39)]
 polyShort <- polychoric(shortScale)
-#Unrotated PCA
+# Unrotated PCA
 faShortScale <- principal(polyShort$rho, nfactors = 2)
 print.psych(faShortScale, digits=2, cut= .4)
-#Rotated PCA
+# Rotated PCA
 faShortScale_rot <- principal(polyShort$rho, nfactors = 2, rotate="oblimin")
 print.psych(faShortScale_rot, digits=2, cut= .4)
 
@@ -72,7 +70,7 @@ print.psych(faShortScale_rot, digits=2, cut= .4)
 item_pos <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
 efaVersion  <-   c(1,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,1,2,2,2,2,2,2,1,2,2,2,2,1,2,2)
 theoryVersion <- c(1,1,2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,2,2,2,2,2,2,2,2,2,2,2,1,2,2)
-dimShort<-cbind(item_pos, efaVersion, theoryVersion)
+dimShort <-cbind(item_pos, efaVersion, theoryVersion)
 
 # EFA result
 cfaEFA <- bfactor(shortScale, efaVersion)
@@ -89,20 +87,31 @@ coef(cfaTheory)
 # IRT
 ###########################
 
+# Create Short Scale - Version 1
 aScale1 <- shortScale[, c(1,2,3,4,5,6,7,8,9,10,12,14,16,17,24,29)]
 
+# Grade Response Model
 triShort1 <- grm(aScale1)
+
+# Estimate Coeficients
 coef(triShort1)
 
+# Plot graphs
 par(mfrow = c(4,4))
 for (i in 1:16) {  
 plot(triShort1, type = "ICC", items = i,  main = "CCI", ylab = "Probabilidade", xlab = "Proficiência", annot=TRUE, legend=FALSE)
 }
 
+# Create Short Scale - Version 2
 aScale2 <- shortScale[, -c(1,2,3,4,5,6,7,8,9,10,12,14,16,17,24,29)]
+
+# # Grade Response Model
 triShort2 <- grm(aScale2)
+
+# Estimate Coeficients
 coef(triShort2)
 
+# Plot graphs
 par(mfrow = c(4,4))
 for (i in 1:15) {  
   plot(triShort2, type = "ICC", items = i,  main = "CCI", ylab = "Probabilidade", xlab = "Proficiência", annot=TRUE, legend=FALSE)
